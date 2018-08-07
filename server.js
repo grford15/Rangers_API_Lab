706e9acf9b29c84403ff625b8b9d48a4ca7162cb
Greg Rutherford <g.rutherford10@gmail.com>
2018-08-07 14:48:13 +0100

    Add CRUD functions

--- server.js
+++ server.js
@@ -8,8 +8,73 @@ server.use(parser.json());
 server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
 
-MongoClient.connect('/mongodb://localhost:27107', function(err, client){
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
 
+  if(err){
+    console.log(err);
+    return;
+  }
+  const db = client.db("rangersdb");
+
+  console.log("Connected to the database");
+
+  //CREATE A PLAYER
+  server.post('/rangers/players', function(req, res){
+    const playersCollection = db.collection('players');
+    const playerToSave = req.body;
+    playersCollection.save(playerToSave, function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      console.log('saved to database');
+      res.status(201);
+      res.json(result.ops[0]);
+    })
+  })
+
+  // READ ALL PLAYERS
+  server.get('/rangers/players', function(req, res){
+    const playersCollection = db.collection('players');
+    playersCollection.find().toArray(function(err, allPlayers){
+      if(err){
+        res.status(500);
+        res.send();
+      }
+      res.json(allPlayers);
+    })
+  })
+
+  // UPDATE A PLAYER
+  server.put('/rangers/players/:id', function(req, res){
+    const playersCollection = db.collection('players');
+    const objectID = ObjectID(req.params.id);
+    const filterObject = {_id: objectID};
+    const updatedPlayer = req.body;
+    playersCollection.update(filterObject, updatedPlayer, function(err, result){
+      if(err){
+        res.status(500);
+        res.send();
+      }
+      res.status(204);
+      res.send();
+    });
+  });
+
+  // DELETE A PLAYER
+  server.delete('/rangers/players/', function(req, res){
+    const filterObject = {}
+    const playersCollection = db.collection('players');
+    playersCollection.deleteMany(filterObject, function(err, result){
+      if(err){
+        res.status(500);
+        res.send()
+      }
+      res.status(204);
+      res.send();
+    })
+  })
 
   server.listen(3000, function(){
     console.log('Listening on port 3000');
